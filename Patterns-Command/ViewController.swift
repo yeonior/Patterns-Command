d197afd17baf2bf63f581a2c816ba2a8a6f2b1bf
Rus <de.mr44@gmail.com>
2022-01-28 12:27:07 +0500

    Added UI

--- Patterns-Command/ViewController.swift
+++ Patterns-Command/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func undoButtonAction(_ sender: UIButton) {
+        
+    }
 }
 
